3ac2a6e7203ca43cf9de6bad45c28543742f124f
Shamshir GSM <shamshir.anees@gmail.com>
2021-02-26 00:26:27 +0530

    fix: - resolve sorting click event

--- src/App/Pages/ViewPlayerView.tsx
+++ src/App/Pages/ViewPlayerView.tsx
@@ -10,7 +10,7 @@ const useStyles = makeStyles((theme: Theme) =>
     }))
 function ViewPlayerView() {
     let parmas: any = useParams();
-    const footballData = useSelector((state: AppState) => state.home.data);
+    const footballData = useSelector((state: AppState) => state.home.sortedData);
     return (
         <div>
             <NavBar />
